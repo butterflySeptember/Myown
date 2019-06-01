41cf454ac137d8b67d0a8121d6c901c4ab7e353d
butterfly_heyi <fox_hy@outlook.com>
2019-06-01 19:55:40 +0800

    增加111

---
+++ MyApplication_two_activity/app/src/main/java/com/example/myapplication/SQL.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import android.os.strictmode.SqliteObjectLeakedViolation;
+
+import java.sql.DatabaseMetaData;
+
+public class SQL extends SqliteObjectLeakedViolation {
+
+    public DatabaseMetaData
+}
