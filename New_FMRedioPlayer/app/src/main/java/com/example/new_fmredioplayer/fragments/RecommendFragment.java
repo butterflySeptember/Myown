f5437e033acced5b5617c416ed36da9192fc7719
butterfly_heyi <fox_hy@outlook.com>
2019-06-19 22:01:18 +0800

    推荐界面内容获取

--- New_FMRedioPlayer/app/src/main/java/com/example/new_fmredioplayer/fragments/RecommendFragment.java
+++ New_FMRedioPlayer/app/src/main/java/com/example/new_fmredioplayer/fragments/RecommendFragment.java
@@ -1,15 +1,65 @@
 package com.example.new_fmredioplayer.fragments;
 
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.new_fmredioplayer.R;
 import com.example.new_fmredioplayer.base.BaseFragment;
+import com.example.new_fmredioplayer.utils.Constants;
+import com.example.new_fmredioplayer.utils.LogUtils;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RecommendFragment extends BaseFragment {
+
+	private final static String TAG = "RecommendFragment";
+
 	public View onSubViewLoaded(LayoutInflater layoutInflater, ViewGroup container){
 		View rootView = layoutInflater.inflate(R.layout.fragment_recommend,container,false);
+
+		//数据获取
+		getRecommendData();
+
+		//返回View
 		return rootView;
+	}
+
+	/**
+	 * 获取推荐内容
+	 */
+	private void getRecommendData() {
+		//封装参数
+		Map<String, String> map = new HashMap<String, String>();
+		//返回参数条数
+		map.put(DTransferConstants.LIKE_COUNT, Constants.RECOMMAND_COUNT+"");
+		CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>() {
+			@Override
+			public void onSuccess(@Nullable GussLikeAlbumList gussLikeAlbumList) {
+				//数据获取成功
+				if (gussLikeAlbumList != null) {
+					List<Album> albumList = gussLikeAlbumList.getAlbumList();
+					if (albumList != null) {
+						LogUtils.d(TAG,"size -->" + albumList.size());
+					}
+
+				}
+			}
+
+			@Override
+			public void onError(int i, String s) {
+				//数据获取出错
+				LogUtils.d(TAG," error -->" + i);
+				LogUtils.d(TAG," errorMsg -->" + s);
+			}
+		});
 	}
 }
---
+++ New_FMRedioPlayer/app/src/main/java/com/example/new_fmredioplayer/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.new_fmredioplayer.utils;
+
+public class Constants {
+
+	//获取的推荐列表专辑数量
+	public static int RECOMMAND_COUNT = 20;
+
+}
